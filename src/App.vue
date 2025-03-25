3126c979d832f0101900ba551f94ff337e0f41d7
Alice Benatti <alice17bee@gmail.com>
2025-03-25 11:43:59 +0100

    base questionnaire

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="bg-base-200 shadow-md">
+    <div class="navbar container mx-auto">
+      <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl">Form Magistrale</a>
+      </div>
+      <div class="flex-none">
+        <nav class="menu menu-horizontal px-1">
+          <RouterLink to="/" class="btn btn-ghost">Home</RouterLink>
+          <RouterLink to="/survey" class="btn btn-ghost">Questionario</RouterLink>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+  <main class="container mx-auto p-4">
+    <RouterView />
+  </main>
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SurveyView from '@/views/SurveyView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    { path: '/survey', name: 'survey', component: SurveyView },
+  ],
+})
+
+export default router
